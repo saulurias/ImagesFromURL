8b82467a62a3413636c8c84de98caf944f3d40aa
Saul Urias <saul_ulises55@hotmail.com>
2016-09-29 21:04:00 -0700

    Initial Commit

---
+++ ImageFromURL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageFromURL
+//
+//  Created by saul ulises urias guzmàn on 29/09/16.
+//  Copyright © 2016 saul ulises urias guzmàn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
